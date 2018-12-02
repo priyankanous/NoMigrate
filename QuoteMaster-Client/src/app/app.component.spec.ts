4aed2ac94cc7b4420a1635682739b62a4bc85ad0
Shaik Mohammed Gouse (BLR GSS) <shaikmohammedg@nousinfo.com>
2018-12-02 18:11:58 +0530

    Section 4 - Registration & Login Implementation

--- QuoteMaster-Client/src/app/app.component.spec.ts
+++ QuoteMaster-Client/src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to QuoteMaster-Client!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Quote Master!');
   }));
 });
